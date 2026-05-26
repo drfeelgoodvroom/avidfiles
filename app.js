bc9f75834bade36db7f23dd292973036cce22a49
Nestor <83480576+drfeelgoodvroom@users.noreply.github.com>
2026-05-25 23:51:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const widget = uploadcare.Widget("[role=uploadcare-uploader]");
 
-  const btn = document.getElementById("uploadBtn");
-
-  btn.addEventListener("click", () => {
-    widget.openDialog();
-  });
-
   widget.onUploadComplete(function(fileInfo){
 
-    if(!fileInfo || !fileInfo.cdnUrl) return;
+    if (!fileInfo || !fileInfo.cdnUrl) return;
 
     alert("Upload successful ✔");
 
-    console.log("File URL:", fileInfo.cdnUrl);
+    console.log("Uploaded file:", fileInfo.cdnUrl);
 
   });
 
