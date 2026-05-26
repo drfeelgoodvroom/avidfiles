3b6d1f02677b781e5833b4c42f7eb1c24b346f50
Nestor <83480576+drfeelgoodvroom@users.noreply.github.com>
2026-05-25 22:41:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 const gallery = document.getElementById("gallery");
 
-const container =
-  document.getElementById("container");
-
 const widget =
   uploadcare.Widget("[role=uploadcare-uploader]");
-
-/* YEARBOOK MODE */
-let yearbook = false;
-
-function toggleMode(){
-
-  yearbook = !yearbook;
-
-  if(yearbook){
-    container.classList.add("yearbook");
-  }else{
-    container.classList.remove("yearbook");
-  }
-
-}
 
 /* LIGHTBOX */
 const lightbox =
@@ -29,70 +11,36 @@ const lightboxImg =
   document.getElementById("lightboxImg");
 
 function openLightbox(url){
-
   lightboxImg.src = url;
-
   lightbox.style.display = "flex";
-
 }
 
 function closeLightbox(){
-
   lightbox.style.display = "none";
-
 }
 
 /* ADD IMAGE */
 function addImage(url){
 
-  if(!yearbook){
+  const tile =
+    document.createElement("div");
 
-    const tile =
-      document.createElement("div");
+  tile.className = "tile";
 
-    tile.className = "tile";
+  const img =
+    document.createElement("img");
 
-    tile.innerHTML =
-      `<img src="${url}"/>`;
+  img.src = url;
 
-    tile.onclick =
-      () => openLightbox(url);
+  tile.appendChild(img);
 
-    gallery.prepend(tile);
+  tile.onclick =
+    () => openLightbox(url);
 
-  }else{
-
-    const entry =
-      document.createElement("div");
-
-    entry.className = "entry";
-
-    entry.innerHTML = `
-      <img src="${url}"/>
-
-      <div>
-        <div class="name">
-          Salinas High AVID 27’
-        </div>
-
-        <div class="sub">
-          AVID Yearbook Archive
-        </div>
-      </div>
-    `;
-
-    entry.onclick =
-      () => openLightbox(url);
-
-    gallery.prepend(entry);
-
-  }
-
+  gallery.prepend(tile);
 }
 
-/* UPLOAD */
+/* UPLOAD HANDLER */
 widget.onUploadComplete(function(fileInfo){
-
   addImage(fileInfo.cdnUrl);
-
 });
